4f84b6a57d9c9405a05acf6754acd13dd35cb8dd
Astrid Geels <moerastrid@gmail.com>
2025-12-14 17:38:30 +0100

    colors ansii

--- src/main/java/ajav/view/terminal/TerminalGui.java
+++ src/main/java/ajav/view/terminal/TerminalGui.java
@@ -4,17 +4,9 @@ import ajav.view.GameView;
 import java.util.Scanner;
 
 public class TerminalGui implements GameView {
-	public static final String ANSI_RESET = "\u001B[0m";
-	public static final String ANSI_BLACK = "\u001B[30m";
-	public static final String ANSI_RED = "\u001B[31m";
-	public static final String ANSI_GREEN = "\u001B[32m";
-	public static final String ANSI_YELLOW = "\u001B[33m";
-	public static final String ANSI_BLUE = "\u001B[34m";
-	public static final String ANSI_PURPLE = "\u001B[35m";
-	public static final String ANSI_CYAN = "\u001B[36m";
-	public static final String ANSI_WHITE = "\u001B[37m";
+	
 
-	Scanner in;
+	static final Scanner in;
 
 	public TerminalGui() {
 		in = new Scanner(System.in);
