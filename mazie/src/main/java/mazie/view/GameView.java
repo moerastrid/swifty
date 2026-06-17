b29f171f8e1fde8d1350619d990d0dbdf0e8d8f0
Astrid Geels <astrid.geels@kvk.nl>
2026-06-17 17:02:24 +0200

    wip gameview

--- mazie/src/main/java/mazie/view/GameView.java
+++ mazie/src/main/java/mazie/view/GameView.java
@@ -1,13 +1,43 @@
 package mazie.view;
 
+import java.util.List;
+
 import mazie.model.Direction;
+import mazie.model.Hero;
+import mazie.model.Monster;
 
 public interface GameView {
+    // default bedacht:
+
     public void showPrompt(String prompt);
 
     public void showError(String error);
 
-	public String getInput();
+    public String getInput();
 
-    public Direction getDirection();
+    // nodig vanuit draw io:
+
+    public void showWelcome();
+
+    public boolean aksNewGame();
+
+    public Hero createHero();
+
+    public Hero selectHero(List<Hero> heroes);
+
+    public boolean confirmHero(Hero hero);
+
+    public void showStartGame();
+
+    public Direction askDirection();
+
+    public void showMonster(Monster monster);
+
+    public boolean askFightMonster();
+
+    public boolean askRunFromMonster();
+
+    
+
+
 }
