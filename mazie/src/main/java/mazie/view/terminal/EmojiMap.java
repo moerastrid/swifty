6bf784b5be712ce3a56c309bcc8c1041d0e37e28
moerastrid@gmail.com <moerastrid@gmail.com>
2026-07-14 20:41:58 +0200

    terug naar java 17, anders is swing niet meer een ding blijkbaar?

--- mazie/src/main/java/mazie/view/terminal/EmojiMap.java
+++ mazie/src/main/java/mazie/view/terminal/EmojiMap.java
@@ -3,19 +3,7 @@ package mazie.view.terminal;
 import java.util.Map;
 
 import mazie.model.HeroType;
-import mazie.model.monster.Bus;
-import mazie.model.monster.Hairsalon;
-import mazie.model.monster.IKEA;
-import mazie.model.monster.Kanteen;
-import mazie.model.monster.Library;
 import mazie.model.monster.Monster;
-import mazie.model.monster.Office;
-import mazie.model.monster.Park;
-import mazie.model.monster.Supermarket;
-import mazie.model.monster.Swimmingpool;
-import mazie.model.monster.TeamBuilding;
-import mazie.model.monster.Teams;
-import mazie.model.monster.Vim;
 
 public class EmojiMap {
     private EmojiMap() {}
@@ -32,19 +20,19 @@ public class EmojiMap {
     }
 
     public static String getEmoji(Monster monster) {
-        return switch (monster) {
-            case Bus _ -> "🚌";
-            case Hairsalon _ -> "✂️";
-            case IKEA _ -> "🛋️";
-            case Kanteen _ -> "🍽️";
-            case Library _ -> "📚";
-            case Office _ -> "🏢";
-            case Park _ -> "🌳";
-            case Supermarket _ -> "🛒";
-            case Swimmingpool _ -> "🏊🏻‍♂️";
-            case TeamBuilding _ -> "🤼‍♂️";
-            case Teams _ -> "👥";
-            case Vim _ -> "⌨️";
+        return switch (monster.getName()) {
+            case "the bus" -> "🚌";
+            case "a hairsalon" -> "✂️";
+            case "IKEA" -> "🛋️";
+            case "your office kanteen" -> "🍽️";
+            case "the library" -> "📚";
+            case "your office" -> "🏢";
+            case "the park" -> "🌳";
+            case "the supermarket (on a saturday)" -> "🛒";
+            case "an indoor swimming pool" -> "🏊🏻‍♂️";
+            case "a teambuilding event" -> "🤼‍♂️";
+            case "a teams meeting" -> "👥";
+            case "vim" -> "⌨️";
             default -> throw new IllegalStateException("Unexpected value: " + (monster));
         };
     }
