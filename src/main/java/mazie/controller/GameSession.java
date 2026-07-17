564a43d35925524764584d3dd890fdf241a83ab5
Astrid Geels <astrid.geels@kvk.nl>
2026-07-17 23:50:10 +0200

    took out playturn from gamesession?

--- src/main/java/mazie/controller/GameSession.java
+++ src/main/java/mazie/controller/GameSession.java
@@ -1,11 +1,7 @@
 package mazie.controller;
 
-import java.util.Random;
-import mazie.model.Artifact;
-import mazie.model.Direction;
-import mazie.model.GameMap;
+import mazie.game.GameEngine;
 import mazie.model.Hero;
-import mazie.model.monster.Monster;
 import mazie.repository.HeroRepository;
 import mazie.view.GameView;
 
@@ -13,126 +9,36 @@ public class GameSession {
 
     private final GameView view;
     private final HeroRepository repository;
+    private final GameEngine engine;
     private final Hero hero;
-    private final GameMap map;
-    private final Random random = new Random();
 
     public GameSession(GameView view, HeroRepository repository, Hero hero) {
         this.view = view;
         this.repository = repository;
+        this.engine = new GameEngine(view, hero);
         this.hero = hero;
-        map = new GameMap(hero.getLevel());
-    }
-
-    public Hero getHero() {
-        return hero;
     }
 
     public void start() {
         view.showStartGame();
 
-        while (turn()) {
-            System.out.println("current map:\n" + map.toString()); //#todo remove
+        while (engine.isPlaying()) {
+            engine.playTurn();
+            repository.update(hero);
 
-            if (heroWins()) {
-                repository.update(hero);
+            if (engine.heroWins()) {
                 view.showEndGame(hero, true);
+                return;
+            }
+            if (engine.heroDies()) {
+                view.showEndGame(hero, false);
+                repository.delete(hero);
                 return;
             }
         }
     }
 
-    private boolean heroWins() {
-        return map.isHeroOnEdge();
-    }
-
     public void close() {
-        if (hero.getId() == 0) {
-            repository.save(hero);
-        } else {
-            repository.update(hero);
-        }
-    }
-
-    // returns true when keep playing, false when game is finito
-    private boolean turn() {
-
-        final var dir = view.askDirection(hero);
-        final var monster = map.getMonsterInDirection(dir);
-        if (monster == null) {
-            map.moveHero(dir);
-            view.showEmptyStep();
-            return true;
-        }
-
-        final boolean feelingAggressive = view.askFightMonster(hero, monster);
-        if (!feelingAggressive) {
-            final var escaped = random.nextBoolean();
-            view.showRunSuccess(monster, escaped);
-            if (escaped) {
-                return true;
-            }
-        }
-
-        final var damageToHero = fight(monster);
-
-        if (hero.isDead()) {
-            view.showEndGame(hero, false);
-            repository.delete(hero);
-            return false;
-        }
-
-        map.removeMonster(dir);
-        map.moveHero(dir);
-        view.showFightSummary(damageToHero, hero, monster);
-
-        final var lvlUp = hero.gainXp(monster.getXpReward());
-        repository.update(hero);
-
-        if (lvlUp) {
-            view.showLevelUp(hero);
-        }
-
-        handleArtifact(monster);
-        return true;
-    }
-
-    private int fight(Monster monster) {
-        var damageToHero = 0;
-        while (!hero.isDead() && !monster.isDead()) {
-            damageToHero += fightRound(monster);
-        }
-        return damageToHero;
-    }
-
-    private int fightRound(Monster monster) {
-        // hero attacks monster
-        monster.takeDamage(hero.getTotalAttack());
-        if (monster.isDead()) {
-            return 0;
-        }
-
-        // monster 50% chance attacks hero
-        if (random.nextBoolean()) {
-            return 0;
-        }
-        return hero.takeDamage(monster.getAttack());
-    }
-
-
-    private void handleArtifact(Monster monster) {
-        final var value = monster.getArtifactValue();
-        final var artifact = Artifact.createArtifact(value);
-
-        if (artifact == null) {
-            return;
-        }
-
-        if (!view.askKeepArtifact(artifact, hero)) {
-            return;
-        }
-
-        hero.setArtifact(artifact);
         repository.update(hero);
     }
 }
--- src/main/java/mazie/game/GameEngine.java
+++ src/main/java/mazie/game/GameEngine.java
@@ -1,25 +1,128 @@
-//package mazie.game;
-//
-//import mazie.model.Artifact;
-//import mazie.model.Direction;
-//import mazie.model.GameMap;
-//import mazie.model.Hero;
-//import mazie.model.monster.Monster;
-//
-//import java.util.Random;
-//
-//public class GameEngine {
-//
-//    private final Hero hero;
-//    private final GameMap map;
-//    private final Random random = new Random();
-//    private Direction pendingDir = null;
-//
-//    public GameEngine(Hero hero) {
-//        this.hero = hero;
-//        this.map = new GameMap(hero.getLevel());
-//    }
-//
+package mazie.game;
+
+import mazie.model.Artifact;
+import mazie.model.GameMap;
+import mazie.model.Hero;
+import mazie.model.monster.Monster;
+
+import java.util.Random;
+import mazie.view.GameView;
+
+public class GameEngine {
+
+    private final GameView view;
+    private final Hero hero;
+    private final GameMap map;
+    private final Random random;
+
+    private boolean playing;
+
+    public GameEngine(GameView view, Hero hero) {
+        this.view = view;
+        this.hero = hero;
+        this.map = new GameMap(hero.getLevel());
+        this.random = new Random();
+        this.playing = true;
+    }
+
+    public boolean isPlaying() {
+        return playing;
+    }
+
+    public boolean heroWins() {
+        return map.isHeroOnEdge();
+    }
+
+    public boolean heroDies() {
+        return hero.isDead();
+    }
+
+    public Hero getHero() {
+        return hero;
+    }
+
+    public void playTurn() {
+        System.out.println("current map:\n" + map.toString()); //#todo remove
+
+        final var dir = view.askDirection(hero);
+        final var monster = map.getMonsterInDirection(dir);
+        if (monster == null) {
+            map.moveHero(dir);
+            view.showEmptyStep();
+            return;
+        }
+
+        final boolean feelingAggressive = view.askFightMonster(hero, monster);
+        if (!feelingAggressive) {
+            final var escaped = random.nextBoolean();
+            view.showRunSuccess(monster, escaped);
+            if (escaped) {
+                return;
+            }
+        }
+
+        final var damageToHero = fight(monster);
+
+        if (hero.isDead()) {
+            playing = false;
+            return;
+        }
+
+        map.removeMonster(dir);
+        map.moveHero(dir);
+        view.showFightSummary(damageToHero, hero, monster);
+
+        final var lvlUp = hero.gainXp(monster.getXpReward());
+
+        if (lvlUp) {
+            view.showLevelUp(hero);
+        }
+
+        handleArtifact(monster);
+    }
+
+    private int fight(Monster monster) {
+        var damageToHero = 0;
+        while (!hero.isDead() && !monster.isDead()) {
+            damageToHero += fightRound(monster);
+        }
+        return damageToHero;
+    }
+
+    private int fightRound(Monster monster) {
+        // hero attacks monster
+        monster.takeDamage(hero.getTotalAttack());
+        if (monster.isDead()) {
+            return 0;
+        }
+
+        // monster 50% chance attacks hero
+        if (random.nextBoolean()) {
+            return 0;
+        }
+        return hero.takeDamage(monster.getAttack());
+    }
+
+    private void handleArtifact(Monster monster) {
+        final var value = monster.getArtifactValue();
+        final var artifact = Artifact.createArtifact(value);
+
+        if (artifact == null) {
+            return;
+        }
+
+        if (!view.askKeepArtifact(artifact, hero)) {
+            return;
+        }
+
+        hero.setArtifact(artifact);
+    }
+}
+
+
+
+
+
 //    public String getMapString() {
 //        return this.map.toString();
 //    }
